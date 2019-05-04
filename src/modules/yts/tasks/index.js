ef6e9c59f770eb2b16863a9b62474f402f8bfcbc
Amr Saber <amr.m.saber.mail@gmail.com>
2019-05-04 11:39:14 +0200

    Add log for yts

--- src/modules/yts/tasks/index.js
+++ src/modules/yts/tasks/index.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const cron = require('node-cron');
 const moment = require('moment');
 
@@ -6,7 +7,10 @@ const checkNewMovies = require('../services/check');
 // running time is at 06:00 +2 UTC regardless of the server's location
 const executingHour = moment.parseZone('06:00 +02:00', 'HH:mm ZZ').utcOffset(2).hours();
 
-const task = cron.schedule(`* ${executingHour} * * *`, async () => await checkNewMovies());
+const task = cron.schedule(`* ${executingHour} * * *`, async () => {
+    console.log(chalk.bgMagenta('Checking YTS Movies'));
+    await checkNewMovies()
+});
 task.stop();
 
 module.exports = task;
