f47662f8078e2e4024863cf0570e707f03932220
Amr Saber <amr.m.saber.mail@gmail.com>
2019-05-08 06:09:06 +0200

    Edit YTS task timing

--- src/modules/yts/tasks/index.js
+++ src/modules/yts/tasks/index.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const checkNewMovies = require('../services/check');
 
 // running time is at 06:00 +2 UTC regardless of the server's location
-const executingHour = moment.parseZone('06:00 +02:00', 'HH:mm ZZ').utcOffset(2).hours();
+const executingHour = moment.parseZone('06:00 +02:00', 'HH:mm ZZ').hours();
 
 const task = cron.schedule(`0 */1 * * *`, async () => {
     if (moment().hours() != executingHour) return;
