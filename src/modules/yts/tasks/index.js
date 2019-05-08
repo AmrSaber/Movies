bf042b1145ccdeeaf88b2315f74e832b4e4103bc
Amr Saber <amr.m.saber.mail@gmail.com>
2019-05-08 05:42:50 +0200

    Edit yts task

--- src/modules/yts/tasks/index.js
+++ src/modules/yts/tasks/index.js
@@ -7,8 +7,10 @@ const checkNewMovies = require('../services/check');
 // running time is at 06:00 +2 UTC regardless of the server's location
 const executingHour = moment.parseZone('06:00 +02:00', 'HH:mm ZZ').utcOffset(2).hours();
 
-const task = cron.schedule(`0 ${executingHour} * * *`, async () => {
+const task = cron.schedule(`0 */1 * * *`, async () => {
+    if (moment().hours() != executingHour) return;
     console.log(chalk.bgMagenta('Checking YTS Movies'));
+
     await checkNewMovies()
 }, { scheduled: false });
 
